a91ddd33ef5a26232fc4c6b3fa0ea2782f7a80d2
Vincent Kuhlmann <vincent.kuhlmann@hotmail.com>
2020-11-08 16:00:06 +0100

    Replace tabs by spaces

--- hub.js
+++ hub.js
@@ -15,52 +15,52 @@ let defaultVoorbladNotes = `\
 let notes = [];
 
 function onDOMReady() {
-	clockUpdateLoop = null;
-	isShowSeconds = false;
-	//clockEvents["13:29:30"] = function () { showSeconds(); };
-	//clockEvents["13:30:00"] = function () { hideSeconds(); };
-	//clockEvents["13:32:00"] = function () { hideVoorbladNotes(); };
-	//clockEvents["22:00:00"] = function () { showVoorbladNotes(); };
+    clockUpdateLoop = null;
+    isShowSeconds = false;
+    //clockEvents["13:29:30"] = function () { showSeconds(); };
+    //clockEvents["13:30:00"] = function () { hideSeconds(); };
+    //clockEvents["13:32:00"] = function () { hideVoorbladNotes(); };
+    //clockEvents["22:00:00"] = function () { showVoorbladNotes(); };
 
-	$("body")[0].addEventListener("keydown", function (e) {
-		if (!isEditing) {
-			if (e.key === "f") {
-				$("body")[0].requestFullscreen();
-				e.preventDefault();
-				e.stopPropagation();
-			} else if (e.key === "2") {
-				if (1 in notes) {
-					notes[1].toggleVisible();
-					e.preventDefault();
-					e.stopPropagation();
-				}
+    $("body")[0].addEventListener("keydown", function (e) {
+        if (!isEditing) {
+            if (e.key === "f") {
+                $("body")[0].requestFullscreen();
+                e.preventDefault();
+                e.stopPropagation();
+            } else if (e.key === "2") {
+                if (1 in notes) {
+                    notes[1].toggleVisible();
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
 
-			} else if (e.key === "1") {
-				if (0 in notes) {
-					notes[0].toggleVisible();
-					e.preventDefault();
-					e.stopPropagation();
-				}
-			} else if (e.key === "r") {
-				resetNotes();
-			}
-		}
-	});
+            } else if (e.key === "1") {
+                if (0 in notes) {
+                    notes[0].toggleVisible();
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+            } else if (e.key === "r") {
+                resetNotes();
+            }
+        }
+    });
 
-	let note2cont = getCookie("notes2");
-	let isOldCookieExists = note2cont !== "";
+    let note2cont = getCookie("notes2");
+    let isOldCookieExists = note2cont !== "";
 
-	if (!isOldCookieExists)
-		note2cont = defaultVoorbladNotes;
+    if (!isOldCookieExists)
+        note2cont = defaultVoorbladNotes;
 
-	notes.push(new Note("note1", null, true, "Test ends at --:--"));
-	notes.push(new Note("note2", null, false, note2cont));
+    notes.push(new Note("note1", null, true, "Test ends at --:--"));
+    notes.push(new Note("note2", null, false, note2cont));
 
-	if (isOldCookieExists) {
-		notes[1].save();
-		setCookie("notes2", "", -1);
-	}
+    if (isOldCookieExists) {
+        notes[1].save();
+        setCookie("notes2", "", -1);
+    }
 
-	updateClock();
-	startClock();
+    updateClock();
+    startClock();
 }
